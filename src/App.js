240efc9e195fa29926c4fee21d574352057a722f
Evans <evansroysir@gmail.com>
2023-03-11 13:19:25 +0300

    Add preload file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 import './App.css';
 import './style.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
-  );
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <ScrollToTop/>
+      </div>
+      
+    </Router>
+  )
 }
 
 export default App;
